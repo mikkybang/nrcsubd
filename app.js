c74e8e43c7ea4c20537e8924b3f44e6b8d211f75
Michael Efemena <mikkybang@localhost.localdomain>
2019-02-18 17:24:37 +0100

    express

--- app.js
+++ app.js
@@ -20,17 +20,10 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/crosser', crosserRoutes);
 app.use('/admin', adminRoutes)
 
-//production mode
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
-  })
-}
+
 //build mode
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 })
 
 
